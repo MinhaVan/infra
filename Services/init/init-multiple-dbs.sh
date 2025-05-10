4e716c559becbf41be99cdc45035d2b8d2981128
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-05-09 18:06:03 -0700

    criando bancos

--- Services/init/init-multiple-dbs.sh
+++ Services/init/init-multiple-dbs.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
-set -e
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth-db') THEN
+        CREATE DATABASE "auth-db";
+    END IF;
+    
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'routes-db') THEN
+        CREATE DATABASE "routes-db";
+    END IF;
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE "auth-db";
-    CREATE DATABASE "routes-db";
-    CREATE DATABASE "payments-db";
-    CREATE DATABASE "pessoas-db";
-EOSQL
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'payments-db') THEN
+        CREATE DATABASE "payments-db";
+    END IF;
+
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pessoas-db') THEN
+        CREATE DATABASE "pessoas-db";
+    END IF;
+END
+$$;
